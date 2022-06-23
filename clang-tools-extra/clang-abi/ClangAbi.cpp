0c79c0b393a3bc324c6b5978ccfd1615b53c26a2
huanghan <huanghan@hyperchain.cn>
2022-06-23 14:14:34 +0800

    delete unnecessary files; print abi methods and their types' name

--- clang-tools-extra/clang-abi/ClangAbi.cpp
+++ clang-tools-extra/clang-abi/ClangAbi.cpp
@@ -38,6 +38,8 @@
 #include <set>
 #include <vector>
 #include <map>
+#include <string>
+#include <sstream>
 #include "llvm/Support/CommandLine.h"
 // #include "ClangAbi.hpp"
 
@@ -101,7 +103,7 @@ static cl::opt<bool> TokensDump("tokens-dump",
                                 cl::desc("dump the preprocessed tokens"),
                                 cl::cat(ClangAbiGen));
 
-namespace /*abigen*/ {
+namespace {
 
 class DumpSyntaxTree : public clang::ASTFrontendAction {
 public:
@@ -146,7 +148,65 @@ public:
 };
 
 // -------------------------------------------abi gen
+inline bool is_template_specialization( const clang::QualType& type, const std::vector<std::string>& names ) {
+  auto check = [&](const clang::Type* pt) {
+      if (auto tst = llvm::dyn_cast<clang::TemplateSpecializationType>(pt)) {
+        if (auto rt = llvm::dyn_cast<clang::RecordType>(tst->desugar())) {
+            if ( names.empty() ) {
+              return true;
+            } else {
+              for ( auto name : names ) {
+                  if ( const auto* decl = rt->getDecl() ) {
+                    if (decl->getName().str() == name) {
+                        return true;
+                    }
+                  }
+              }
+            }
+        }
+      }
+    return false;
+  };
+  bool is_specialization = false;
+  if (auto pt = llvm::dyn_cast<clang::ElaboratedType>(type.getTypePtr())) {
+      is_specialization = check(pt->desugar().getTypePtr());
+  } else
+      is_specialization = check(type.getTypePtr());
 
+  return is_specialization;
+}
+
+inline const bool isPrimitive(const clang::Type* t) {
+  if(t->isBooleanType() || t->isCharType() || 
+     t->isIntegerType() || t->isFloatingType()) 
+    return true;
+  return false;
+}
+
+static void addPrimitive(const clang::Type* t) {
+
+}
+
+static void addClassType(const clang::Type* t) {
+  
+}
+
+static void addStructType(const clang::Type* t) {
+  
+}
+
+static void addVectorType(const clang::Type* t) {
+  
+}
+
+static void addArrayType(const clang::Type* t) {
+  
+}
+
+class TypeInf;
+class AbiGen;
+
+// record abi methods
 class MethodSig {
 private:
   std::string methodName;
@@ -157,74 +217,177 @@ public:
   std::string getName() {
     return methodName;
   }
-  MethodSig(std::string methodName) {
-    methodName = std::string(methodName);
+  // constructor
+  MethodSig(std::string name) {
+    methodName = name.c_str();
     inputTypes = vector<std::string>();
     returnTypes = vector<std::string>();
   }
-  void addInput(std::string inputType) {
-    inputTypes.push_back(inputType);
+  void addOutputType(const clang::QualType& t) {
+    // getNonReferenceType: If Type is a reference type (e.g., const int&),
+    // returns the type that the reference refers to ("const int").
+    returnTypes.push_back(t.getNonReferenceType().getAsString());
   }
-  void addOutput(std::string outputType) {
-    returnTypes.push_back(outputType);
+  void addInputType(const clang::QualType& t) {
+    // todo: check is void
+    inputTypes.push_back(t.getNonReferenceType().getAsString());
+  }
+  // for debug
+  string toString() {
+    std::stringstream ss;
+    ss << "method name:\n"
+       << "\t" << methodName << "\n";
+    if(!inputTypes.empty()) {
+      ss << "input type:\n";
+      for(auto s: inputTypes) {
+        ss << "\t" << s << "\n";
+      }
+    }
+    if(!returnTypes.empty()) {
+      ss << "output type:\n";
+      for(auto s: returnTypes) {
+        ss << "\t" << s << "\n";
+      }
+    }
+    return ss.str();
   }
 };
 
-class TypeInf;
-class TypeInf{
+// record abi type information
+typedef struct TypeInf{
   std::string typeName;
   bool isPrimitive;
-  std::vector<TypeInf> fields;
-};
+  std::vector<std::string> fields;
+}TypeInf;
 
 class AbiGen {
   // methods and their param
-  std::map<std::string, MethodSig> abiMethods;
+  std::map<std::string, MethodSig*> abiMethods;
   // type set, only used for abi generate, ignore other type
-  std::map<std::string, TypeInf> abiTypes;
+  std::map<std::string, TypeInf*> abiTypeMap;
 
-public:
-  void addMethod(MethodSig method) {
-    // no check exist or not, just replace
-    abiMethods[method.getName()] = method;
+public: 
+  static AbiGen& get() {
+      static AbiGen ag;
+      return ag;
+  }
+  std::map<std::string, MethodSig*> methods() {
+    return abiMethods;
+  }
+
+  void addAbiType(const clang::QualType& t) {
+    // t->dump();
+    // getTypePtr return pt
+    string typeName = t.getNonReferenceType().getAsString();
+    llvm::outs() << "----------type name:" << typeName << "\n";
+    // this type is recorded
+    if(abiTypeMap.find(typeName) != abiTypeMap.end())
+      return;
+    // auto tp = t.getNonReferenceType().getTypePtr(); 
+    auto tp = t.getTypePtr(); 
+
+    // =========judge type and deal
+    // you should judge template first
+    if(is_template_specialization(t, {"vector", "array"})) {
+      llvm::outs() << "\ttemplate:" << typeName << "\n";
+    } 
+    if(tp->isVectorType()) {
+      llvm::outs() << "\tvector:" << typeName << "\n";
+      addVectorType(tp);
+    }
+    if(tp->isArrayType()) {
+        llvm::outs() << "\tarray:" << typeName << "\n";
+        addArrayType(tp);
+    }
+    if(isPrimitive(tp)) {
+      if(tp->isCharType())llvm::outs() << "\tchar:" << typeName << "\n";
+      if(tp->isIntegerType())llvm::outs() << "\tinteger:" << typeName << "\n", t->dump();
+      if(tp->isFloatingType())llvm::outs() << "\tinteger:" << typeName << "\n";
+      if(tp->isBooleanType())llvm::outs() << "\tbool:" << typeName << "\n";
+      // llvm::outs() << "\tprimitive:" << typeName << "\n";
+      addPrimitive(tp);
+    }
+    if(tp->isClassType()) {
+      llvm::outs() << "\tclass:" << typeName << "\n";
+      addClassType(tp);
+    }
+    if(tp->isStructureType()) {
+      llvm::outs() << "\tstruct:" << typeName << "\n";
+      addStructType(tp);
+    }
+    if(tp->isVoidType()) {
+      llvm::outs() << "\tvoid type:" << typeName << "\n";
+    }
+    if(tp->isBuiltinType())llvm::outs() << "\tbuiltin type:" << typeName << "\n";
+    if(tp->isPointerType())llvm::outs() << "\tpointer:" << typeName << "\n";
+    if(tp->isAlignValT())llvm::outs() << "\tAlignValT:" << typeName << "\n";
+    if(tp->isElaboratedTypeSpecifier())llvm::outs() << "\tElaborated:" << typeName << "\n";
+    {
+      // llvm::outs() << "\tdefault:not a type abi allowed\n";
+    }
+  }
+
+  // handle methods (or functions)
+  void addAbiMethod(FunctionDecl* decl) {
+    decl->dumpColor();
+    // get method name
+    auto methodName = decl->getNameAsString();
+    llvm::outs() << "add abi method: "
+                 << methodName << "\n";
+    // check if it already exists
+    if(abiMethods.find(methodName) != abiMethods.end())
+      return;
+    // new MethodSig class
+    MethodSig* methodSig = new MethodSig(methodName);
+    //  input param type 
+    for (auto param : decl->parameters()) {
+      // add QualType input
+      methodSig->addInputType(param->getType());
+      // deal type
+      addAbiType(param->getType());
+    }
+    // return, c++ only has one return
+    methodSig->addOutputType(decl->getReturnType());
+    // deal return type
+    addAbiType(decl->getReturnType());
+    // insert into abiMethods
+    abiMethods[methodName] = methodSig;
   }
   
 };
 
+//-------------------visitor
 class ABIGenClassVisitor : public RecursiveASTVisitor<ABIGenClassVisitor> {
 public:
-  // explicit ABIGenClassVisitor(CompilerInstance *CI) {
-  //   get_error_emitter().set_compiler_instance(CI);
-  // }
+  AbiGen& abiGen = AbiGen::get();
+  
   explicit ABIGenClassVisitor(ASTContext *Context)
     : Context(Context) {}
+  // it is said clang won't visit template instance by default
+  bool shouldVisitTemplateInstantiations() const { return true;}
 
   // c++ struct/union/class
-  bool VisitCXXRecordDecl(CXXRecordDecl *Declaration) {
+  virtual bool VisitCXXRecordDecl(CXXRecordDecl *Declaration) {
     // For debugging, dumping the AST nodes will show which nodes are already
     // being visited.
-    // Declaration->dump();
     if(Declaration->hasAttr<FvmContractAttr>()) {
-      FullSourceLoc FullLocation = Context->getFullLoc(Declaration->getBeginLoc());
-      if (FullLocation.isValid())
-        llvm::outs() << "*********xxxxxxxxxxxxxxxx------------Found declaration at "
-                     << FullLocation.getSpellingLineNumber() << ":"
-                     << FullLocation.getSpellingColumnNumber() << 
-                     "*********xxxxxxxxxxxxxxxx------------" << "\n";
-    }else Declaration->dump();
+      Declaration->dump();
+    }
     // The return value indicates whether we want the visitation to proceed.
     // Return false to stop the traversal of the AST.
     return true;
   }
-  bool VisitCXXMethodDecl(clang::CXXMethodDecl* decl) {
-    decl->dump();
-    for (auto param : decl->parameters()) {
-      param->dump();
-    }
+  // c++ method, no check now
+  virtual bool VisitCXXMethodDecl(clang::CXXMethodDecl* decl) {
+    // decl->dump();
     return true;
   }
+  // function, CXXMethodDecl extends FunctionDecl, so this one is enough
   virtual bool VisitFunctionDecl(FunctionDecl* func_decl) {
-    func_decl->dump();
+    if(func_decl->hasAttr<FvmContractAttr>()) {
+      abiGen.addAbiMethod(func_decl);
+    }
+    // func_decl->dump();
     return true;
   }
 
@@ -328,5 +491,11 @@ int main(int argc, const char **argv) {
   else
     FrontendFactory = newFrontendActionFactory(&CheckFactory);
 
-  return Tool.run(FrontendFactory.get());
+  
+  Tool.run(FrontendFactory.get());
+  auto methodMap = AbiGen::get().methods();
+  for(auto methodIter = methodMap.begin(); methodIter != methodMap.end(); methodIter++) {
+    llvm::outs() << (methodIter->second)->toString() << "\n";
+  }
+  // return Tool.run(FrontendFactory.get());
 }
