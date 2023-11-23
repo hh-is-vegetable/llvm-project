22402e696c5d7d0fa12dd61151d14ba24d56798d
huanghan <huangh_xh@outlook.com>
2023-11-23 23:38:34 +0800

    change function type for __imported_host_set_value

--- lld/wasm/Driver.cpp
+++ lld/wasm/Driver.cpp
@@ -614,9 +614,10 @@ static void createSyntheticSymbols() {
   createUndefinedFunction(
         "__imported_host_get_value", StringRef("__get_value"), StringRef("__host"), &i64ArgSignaturei32
       );
-  static WasmSignature i32i64ArgSignature = {{}, {ValType::I32, ValType::I64}};
+  // addr-key, base, size, attr, imm
+  static WasmSignature i32x5ArgSignature = {{}, {ValType::I32, ValType::I32, ValType::I32, ValType::I32, ValType::I32}};
   createUndefinedFunction(
-      "__imported_host_set_value", StringRef("__set_value"), StringRef("__host"), &i32i64ArgSignature
+      "__imported_host_set_value", StringRef("__set_value"), StringRef("__host"), &i32x5ArgSignature
       );
 
   static WasmSignature nullSignature = {{}, {}};
